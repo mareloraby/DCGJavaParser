f9fb745d1800eef86c89ee6f13aa917cc997b483
Maryam ElOraby <maryam.eloraby@gmail.com>
2023-04-16 00:20:30 +0200

    fixed errors

--- DCGJava.pl
+++ DCGJava.pl
@@ -1,52 +1,49 @@
-s(s(JC)) --> java_code(JC).
-
-java_code( jc(As)) --> assignment_statement(As).
-java_code( jc(Cs) ) --> conditional_statement(Cs).
-java_code( jc(Ls) ) --> loop(Ls).
+s( s(As)) --> assignment_statement(As).
+s( s(Cs) ) --> conditional_statement(Cs).
+s( s(Ls) ) --> loop(Ls).
 
 % ---------------------------------------------------------
 
-assignment_statement(as(JI,=,AE,';')) --> java_identifier(JI), [=], arithmetic_expression(AE), [';'].
+assignment_statement(assignStatement(JI,=,AE,';')) --> java_identifier(JI), [=], arithmetic_expression(AE), [';'].
 
-arithmetic_expression( ae(AT,AER) ) --> arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression( exprs(AT,AER) ) --> arithmetic_term(AT), arithmetic_expression_rest(AER).
 
-arithmetic_expression_rest( aer(+, AT, AER) ) --> [+], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( aer(-, AT, AER) ) --> [-], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( aer(*, AT, AER) ) --> [*], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( aer(/, AT, AER) ) --> [/], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( aer('%', AT, AER) ) --> ['%'], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( aer() ) --> [].
+arithmetic_expression_rest( exprsRest(+, AT, AER) ) --> [+], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression_rest( exprsRest(-, AT, AER) ) --> [-], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression_rest( exprsRest(*, AT, AER) ) --> [*], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression_rest( exprsRest(/, AT, AER) ) --> [/], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression_rest( exprsRest('%', AT, AER) ) --> ['%'], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression_rest( exprsRest() ) --> [].
 
-arithmetic_term( at(T) ) --> term(T).
-arithmetic_term( at('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
+arithmetic_term( arithTrm(T) ) --> term(T).
+arithmetic_term( bracketTrm('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
 
-term( t(JI) ) --> java_identifier(JI).
-term( t(Uil) ) --> unsigned_int_literal(Uil).
+term( trm(JI) ) --> java_identifier(JI).
+term( trm(Uil) ) --> unsigned_int_literal(Uil).
 
-java_identifier( ji(X) ) --> [X], {atom_chars(X,Chars), Chars\=[], Chars = [FirstChar|_], \+code_type(FirstChar, digit), maplist(java_identifier_char,Chars)}.
-
+java_identifier( id(X) ) --> [X], {atom_chars(X,Chars), Chars\=[], Chars = [FirstChar|_], \+code_type(FirstChar, digit), maplist(java_identifier_char,Chars)}.
 java_identifier_char(C) :- code_type(C, alpha).
 java_identifier_char(C) :- code_type(C, digit).
 java_identifier_char('_').
 
-unsigned_int_literal( uil(X) ) --> [X], {integer(X)}.
+unsigned_int_literal( int(X) ) --> [X], {integer(X)}.
 
 % ---------------------------------------------------------
 
-conditional_statement(cs('if','(', Cn, ')', Ifb, Else)) --> ['if'], ['('], condition(Cn), [')'], if_body(Ifb), else_condition(Else).
+conditional_statement(ifStatement('if','(', Cn, ')', Ifb, Else)) --> ['if'], ['('], condition(Cn), [')'], if_body(Ifb), else_condition(Else).
 
-if_body( ibody(AS) ) --> assignment_statement(AS).
-if_body( ibody(CS) ) --> conditional_statement(CS).
+if_body( ifbody(AS) ) --> assignment_statement(AS).
+if_body( ifbody(CS) ) --> conditional_statement(CS).
 
-else_condition(ec( 'else', Ifb)) --> ['else'], if_body(Ifb).
-else_condition(ec()) --> [].
+else_condition(elseBody( 'else', Ifb)) --> ['else'], if_body(Ifb).
+else_condition(elseBody()) --> [].
 
 % ---------------------------------------------------------
 
 loop( while('while','(', Cn,')',Lb) ) --> ['while'], ['('], condition(Cn), [')'], loop_body(Lb).
 
-loop_body( lbody(AS)) --> assignment_statement(AS).
-loop_body( lbody(W)) --> loop(W).
+loop_body( loopBody(AS)) --> assignment_statement(AS).
+loop_body( loopBody(W)) --> loop(W).
 
 % ---------------------------------------------------------
 
