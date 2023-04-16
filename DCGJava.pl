f88680e2401db8095941087f9c0334a84718d2eb
Maryam ElOraby <maryam.eloraby@gmail.com>
2023-04-16 14:43:00 +0200

    reduns

--- DCGJava.pl
+++ DCGJava.pl
@@ -1,6 +1,7 @@
 s( s(JC1)) --> java_code(JC1).
 s( s(JC1, JC2) ) --> java_code(JC1), java_code(JC2).
 s( s(JC1, JC2, JC3)) --> java_code(JC1), java_code(JC2), java_code(JC3).
+s( s(JC1, JC2, JC3, JC4)) --> java_code(JC1), java_code(JC2), java_code(JC3), java_code(JC4).
 
 % ---------------------------------------------------------
 
@@ -10,27 +11,17 @@ java_code(Cs) --> conditional_statement(Cs).
 
 % ---------------------------------------------------------
 
-assignment_statement(assignStatement(JI,=,AE,';')) --> java_identifier(JI), [=], arithmetic_expression(AE), [';'].
-
 arithmetic_expression( AT ) --> arithmetic_term(AT).
-arithmetic_expression( exprs(AT,AER) ) --> arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expression( exprs(AT,AER) ) --> arithmetic_term(AT), arithmetic_expr_rest(AER).
 
-arithmetic_expression_rest( exprsRest(+, AT) ) --> [+], arithmetic_term(AT).
-arithmetic_expression_rest( exprsRest(+, AT, AER) ) --> [+], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( exprsRest(-, AT) ) --> [-], arithmetic_term(AT).
-arithmetic_expression_rest( exprsRest(-, AT, AER) ) --> [-], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( exprsRest(*, AT) ) --> [*], arithmetic_term(AT).
-arithmetic_expression_rest( exprsRest(*, AT, AER) ) --> [*], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( exprsRest(/, AT) ) --> [/], arithmetic_term(AT).
-arithmetic_expression_rest( exprsRest(/, AT, AER) ) --> [/], arithmetic_term(AT), arithmetic_expression_rest(AER).
-arithmetic_expression_rest( exprsRest('%', AT) ) --> ['%'], arithmetic_term(AT).
-arithmetic_expression_rest( exprsRest('%', AT, AER) ) --> ['%'], arithmetic_term(AT), arithmetic_expression_rest(AER).
+arithmetic_expr_rest( exprsRest(OP, AT) ) --> arithmetic_operator(OP), arithmetic_term(AT).
+arithmetic_expr_rest( exprsRest(OP, AT, AER) ) --> arithmetic_operator(OP), arithmetic_term(AT), arithmetic_expr_rest(AER).
 
 arithmetic_term( JI ) --> java_identifier(JI).
 arithmetic_term( Uil ) -->unsigned_int_literal(Uil).
 arithmetic_term( bracketTrm('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
 
-java_identifier( id(X) ) --> [X], { X \== while, X \== if, X\== else, atom_chars(X,Chars), Chars\=[], Chars = [FirstChar|_], \+code_type(FirstChar, digit), maplist(java_identifier_char,Chars)}.
+java_identifier( id(X) ) --> [X], {atom_chars(X,Chars), Chars\=[], Chars = [FirstChar|_], \+code_type(FirstChar, digit), maplist(java_identifier_char,Chars)}.
 java_identifier_char(C) :- code_type(C, alpha).
 java_identifier_char(C) :- code_type(C, digit).
 java_identifier_char('_').
@@ -38,27 +29,32 @@ java_identifier_char('_').
 unsigned_int_literal( int(X) ) --> [X], {integer(X)}.
 
 % ---------------------------------------------------------
+loop( whileLoop(while,'(', Cn,')',Lb) ) --> [while], ['('], condition(Cn), [')'], loop_body(Lb).
 
 conditional_statement(ifStatement('if','(', Cn, ')', Ifb)) --> ['if'], ['('], condition(Cn), [')'], if_body(Ifb).
 conditional_statement(ifStatement('if','(', Cn, ')', Ifb, Else)) --> ['if'], ['('], condition(Cn), [')'], if_body(Ifb), else_condition(Else).
 else_condition(elseBody( 'else', Ifb)) --> ['else'], if_body(Ifb).
 
-if_body( ifbody(AS) ) --> assignment_statement(AS).
-if_body( ifbody(CS) ) --> conditional_statement(CS).
-
-% ---------------------------------------------------------
-
-loop( whileLoop(while,'(', Cn,')',Lb) ) --> [while], ['('], condition(Cn), [')'], loop_body(Lb).
+assignment_statement(assignStatement(JI,=,AE,';')) --> java_identifier(JI), [=], arithmetic_expression(AE), [';'].
 
 loop_body( loopBody(AS)) --> assignment_statement(AS).
 loop_body( loopBody(W)) --> loop(W).
 
-% ---------------------------------------------------------
+if_body( ifbody(AS) ) --> assignment_statement(AS).
+if_body( ifbody(CS) ) --> conditional_statement(CS).
 
-condition( cdn(AE1,'==',AE2) ) --> arithmetic_expression(AE1), ['=='], arithmetic_expression(AE2).
-condition( cdn(AE1,'!=',AE2) ) --> arithmetic_expression(AE1), ['!='], arithmetic_expression(AE2).
-condition( cdn(AE1,'<=',AE2) ) --> arithmetic_expression(AE1), ['<='], arithmetic_expression(AE2).
-condition( cdn(AE1,'>=',AE2) ) --> arithmetic_expression(AE1), ['>='], arithmetic_expression(AE2).
-condition( cdn(AE1,'<',AE2) ) -->  arithmetic_expression(AE1), ['<'],  arithmetic_expression(AE2).
-condition( cdn(AE1,'>',AE2) ) -->  arithmetic_expression(AE1), ['>'],  arithmetic_expression(AE2).
+condition( cdn(AE1,Op,AE2) ) -->  arithmetic_expression(AE1), cprn_operator(Op), arithmetic_expression(AE2).
 
+
+cprn_operator('==') --> ['=='].
+cprn_operator('!=') --> ['!='].
+cprn_operator('<=') --> ['<='].
+cprn_operator('>=') --> ['>='].
+cprn_operator('<') --> ['<'].
+cprn_operator('>') --> ['>'].
+
+arithmetic_operator( + ) --> [+].
+arithmetic_operator( - ) --> [-].
+arithmetic_operator( / ) --> [/].
+arithmetic_operator( * ) --> [*].
+arithmetic_operator( '%' ) --> ['%'].
