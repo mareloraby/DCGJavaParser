03cd2f1d57f56ff54d8ed182769cfa17f65819a5
Maryam ElOraby <maryam.eloraby@gmail.com>
2023-04-16 21:22:19 +0200

    reordering

--- DCGJava.pl
+++ DCGJava.pl
@@ -7,29 +7,6 @@ s( s(JC1, JC2, JC3, JC4)) --> java_code(JC1), java_code(JC2), java_code(JC3), ja
 
 java_code(Stmt) --> loop(Stmt) | conditional_statement(Stmt) | assignment_statement(Stmt).
 
-% ---------------------------------------------------------
-
-arithmetic_expression( AT ) --> term(AT).
-arithmetic_expression( exprs(AT,AER) ) --> term(AT), arithmetic_expr_rest(AER).
-
-arithmetic_expr_rest( exprsRest(OP, AT) ) --> arithmetic_operator(OP), term(AT).
-arithmetic_expr_rest( exprsRest(OP, AT, AER) ) --> arithmetic_operator(OP), term(AT), arithmetic_expr_rest(AER).
-
-term( JI ) --> java_identifier(JI).
-term( Uil ) --> unsigned_int_literal(Uil).
-term( paren('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
-
-java_identifier(id([H|T])) --> [H], { is_alpha(H); H == '_' }, identifier_cont(T).
-identifier_cont([H|T]) --> [H], { is_alnum(H) }, identifier_cont(T).
-identifier_cont([]) --> [].
-
-is_alpha(H) :- code_type(H, alpha).
-is_alnum(H) :- code_type(H, alnum); (integer(H)).
-
-unsigned_int_literal( int(X) ) --> [X], {integer(X)}.
-
-% ---------------------------------------------------------
-
 loop( whileLoop(while,'(', Cn,')',Lb) ) --> [w,h,i,l,e], ['('], condition(Cn), [')'], loop_body(Lb).
 
 conditional_statement(ifStatement(if,'(', Cn, ')', Ifb)) --> [i,f], ['('], condition(Cn), [')'], if_body(Ifb).
@@ -50,6 +27,25 @@ cprn_operator('<=') --> [<,=].
 cprn_operator('>=') --> [>,=].
 cprn_operator('<') --> [<].
 cprn_operator('>') --> [>].
+
+arithmetic_expression( AT ) --> term(AT).
+arithmetic_expression( exprs(AT,AER) ) --> term(AT), arithmetic_expr_rest(AER).
+
+arithmetic_expr_rest( exprsRest(OP, AT) ) --> arithmetic_operator(OP), term(AT).
+arithmetic_expr_rest( exprsRest(OP, AT, AER) ) --> arithmetic_operator(OP), term(AT), arithmetic_expr_rest(AER).
+
+term( JI ) --> java_identifier(JI).
+term( Uil ) --> unsigned_int_literal(Uil).
+term( paren('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
+
+java_identifier(id([H|T])) --> [H], { is_alpha(H); H == '_' }, identifier_cont(T).
+identifier_cont([H|T]) --> [H], { is_alnum(H) }, identifier_cont(T).
+identifier_cont([]) --> [].
+
+is_alpha(H) :- code_type(H, alpha).
+is_alnum(H) :- code_type(H, alnum); (integer(H)).
+
+unsigned_int_literal( int(X) ) --> [X], {integer(X)}.
 
 arithmetic_operator( + ) --> [+].
 arithmetic_operator( - ) --> [-].
