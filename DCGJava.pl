7d159f516561f92ef02560cbcfa8d185409f01d8
Maryam ElOraby <maryam.eloraby@gmail.com>
2023-04-27 20:02:44 +0200

    test cases tested

--- DCGJava.pl
+++ DCGJava.pl
@@ -7,11 +7,11 @@ s( s(JC1, JC2, JC3, JC4)) --> java_code(JC1), java_code(JC2), java_code(JC3), ja
 
 java_code(Stmt) --> loop(Stmt) | conditional_statement(Stmt) | assignment_statement(Stmt).
 
-loop( whileLoop(while,'(', Cn,')',Lb) ) --> [w,h,i,l,e], ['('], condition(Cn), [')'], loop_body(Lb).
+loop( whileLoop(while,'(', Cn,')',Lb) ) --> [while], ['('], condition(Cn), [')'], loop_body(Lb).
 
-conditional_statement(ifStatement(if,'(', Cn, ')', Ifb)) --> [i,f], ['('], condition(Cn), [')'], if_body(Ifb).
-conditional_statement(ifStatement(if,'(', Cn, ')', Ifb, Else)) --> [i,f], ['('], condition(Cn), [')'], if_body(Ifb), else_condition(Else).
-else_condition(elseBody( else, Ifb)) --> [e,l,s,e], if_body(Ifb).
+conditional_statement(ifStatement(if,'(', Cn, ')', Ifb)) --> [if], ['('], condition(Cn), [')'], if_body(Ifb).
+conditional_statement(ifStatement(if,'(', Cn, ')', Ifb, Else)) --> [if], ['('], condition(Cn), [')'], if_body(Ifb), else_condition(Else).
+else_condition(elseBody( else, Ifb)) --> [else], if_body(Ifb).
 
 assignment_statement(assign(JI,=,AE,';')) --> java_identifier(JI), [=], arithmetic_expression(AE), [';'].
 
@@ -21,12 +21,12 @@ if_body( ifbody(Stmt) ) --> assignment_statement(Stmt) | conditional_statement(S
 
 condition( cdn(AE1,Op,AE2) ) -->  arithmetic_expression(AE1), cprn_operator(Op), arithmetic_expression(AE2).
 
-cprn_operator('==') --> [=,=].
-cprn_operator('!=') --> ['!',=].
-cprn_operator('<=') --> [<,=].
-cprn_operator('>=') --> [>,=].
-cprn_operator('<') --> [<].
-cprn_operator('>') --> [>].
+cprn_operator('==') --> ['=='].
+cprn_operator('!=') --> ['!='].
+cprn_operator('<=') --> ['<='].
+cprn_operator('>=') --> ['>='].
+cprn_operator('<') --> ['<'].
+cprn_operator('>') --> ['>'].
 
 arithmetic_expression( AT ) --> term(AT).
 arithmetic_expression( exprs(AT,AER) ) --> term(AT), arithmetic_expr_rest(AER).
@@ -38,8 +38,8 @@ term( JI ) --> java_identifier(JI).
 term( Uil ) --> unsigned_int_literal(Uil).
 term( paren('(', AE, ')')) --> ['('], arithmetic_expression(AE), [')'].
 
-java_identifier(id([H|T])) --> [H], { is_alpha(H); H == '_' }, identifier_cont(T).
-identifier_cont([H|T]) --> [H], { is_alnum(H) }, identifier_cont(T).
+java_identifier(id([H|T])) --> [H], {  (H\=='while', H \== 'if', H\== 'else'), (is_alpha(H); H == '_') }, identifier_cont(T).
+identifier_cont([H|T]) --> [H], { ((H \== '!='), (H \== '<='), (H \== '>='), (H \== '==') ), is_alnum(H)  }, identifier_cont(T).
 identifier_cont([]) --> [].
 
 is_alpha(H) :- code_type(H, alpha).
